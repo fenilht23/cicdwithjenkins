e78e409b7311739693710fc69d580f0086e6f122
fenilht23 <96934605+fenilht23@users.noreply.github.com>
2024-06-21 21:12:05 +0530

    Update ChildComponent.js

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -1,15 +1,5 @@
 const ChildComponent = ({ data, loading }) => {
     return (
-        // <div>
-        //     <h1>Child Component</h1>
-        //     {
-        //         loading ? (
-        //             <div class="spinner-border text-info" role="status">
-        //                 <span class="visually-hidden">Loading...</span>
-        //             </div>
-        //         ) : ( data )
-        //     }
-        // </div>
         <div className="card text-center mt-5 mx-5">
             <div className="card-header">
                 <h1>Child Component</h1>

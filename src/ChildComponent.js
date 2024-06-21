e1aa85dae5be06e760270c1ec0b19aa0eeee356a
fenilht23 <96934605+fenilht23@users.noreply.github.com>
2024-06-21 21:12:55 +0530

    Update ChildComponent.js

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -9,7 +9,7 @@ const ChildComponent = ({ data, loading }) => {
                     {
                         loading ? (
                             <div class="spinner-border text-info" role="status">
-                                <span class="visually-hidden">Loading...</span>
+                                <span class="visually-hidden">Loading Component...</span>
                             </div>
                         ) : (data)
                     }
